742d77b8f861e5f0889f385b8c3a4f47cde5370b
Diogo Nadalini <diogo.nadalini@gmail.com>
2024-10-16 09:14:46 -0300

    ex1_arrumado

--- funcoes.py
+++ funcoes.py
@@ -10,5 +10,5 @@ def define_posicoes (linha, coluna, orientacao, tamanho):
             posicoes_lista.append(posicoes)
     return posicoes_lista
 
-print (define_posicoes(2, 4, 'vertical', 3))
+
 
