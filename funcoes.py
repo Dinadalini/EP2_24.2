552aed3062e8f06ac19b52bf1ca756f83c96c707
Diogo Nadalini <diogo.nadalini@gmail.com>
2024-10-16 14:40:29 -0300

    ex6.v1

--- funcoes.py
+++ funcoes.py
@@ -1,9 +1,11 @@
 def define_posicoes (linha, coluna, orientacao, tamanho):
     posicoes_lista = []
+
     if orientacao == 'horizontal':
         for i in range(tamanho):
             posicoes = [linha, coluna+i]
             posicoes_lista.append(posicoes)
+
     elif orientacao == 'vertical':
         for i in range(tamanho):
             posicoes = [linha+i, coluna]
@@ -21,6 +23,7 @@ def preenche_frota (frota, nome_navio, linha, coluna, orientacao, tamanho):
     return frota
 
 def faz_jogada (tabuleiro, linha, coluna):
+
     if tabuleiro[linha][coluna] == 1:
         tabuleiro[linha][coluna] = 'X'
     else:
@@ -41,6 +44,7 @@ def posiciona_frota (frota):
       [0,0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0,0],
   ]
+    
     for navios in frota.keys():
         for posicoes in frota[navios]:
             for posicao in posicoes:
@@ -51,6 +55,7 @@ def posiciona_frota (frota):
 
 def afundados(frota, tabuleiro):
     navios_afundados = 0
+
     for navio in frota:
         for navio in frota[navio]:
             afundado = True
@@ -64,3 +69,19 @@ def afundados(frota, tabuleiro):
     
     return navios_afundados
 
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+
+    navio_atual = define_posicoes(linha, coluna, orientacao, tamanho)
+
+    for posicao_atual in navio_atual:
+        linha_atual, coluna_atual = posicao_atual
+        if linha_atual < 0 or linha_atual > 9 or coluna_atual < 0 or coluna_atual > 9:
+            return False
+        
+    for navios in frota:
+        for navio in frota[navios]:
+            for posicao_ocupada in navio:
+                if posicao_ocupada in navio_atual:
+                    return False
+
+    return True
