680dd6b03dc684caaa7ab2a83ab0366c714495de
Diogo Nadalini <diogo.nadalini@gmail.com>
2024-10-23 14:42:43 -0300

    ex7.v2

--- programa.py
+++ programa.py
@@ -12,6 +12,7 @@ tamanho_navio={"porta-aviões":[1, 4], "navio-tanque":[2, 3], "contratorpedeiro"
 for nome, dados in tamanho_navio.items():
     j=1
     while j<=dados[0]:
+        #pede as informacoes do navio que será colocado
         print(f'Insira as informações referentes ao navio {nome} que possui tamanho {dados[1]}')	
         linha_atual= int(input("Qual linha deseja colocar o navio? "))
         coluna_atual= int(input("Qual coluna deseja colocar o navio? "))
@@ -21,16 +22,13 @@ for nome, dados in tamanho_navio.items():
                 orientacao="vertical"
             elif orientacao==2:
                 orientacao="horizontal"
-        #navio_atual_define_posicoes = define_posicoes(linha_atual, coluna_atual, orientacao, dados[1])
         tamanho= dados[1]
-        #print(tamanho)
         navio_atual_posicao_valida = posicao_valida(frota, linha_atual, coluna_atual, orientacao, tamanho)
         if navio_atual_posicao_valida== True:
-            #dado true, tem colocar as posicoes ocupadas na lista de posicoes da frota
+            # posicoes_validadas=define_posicoes(linha_atual, coluna_atual, orientacao, tamanho) 
+            frota[nome].append(define_posicoes(linha_atual, coluna_atual, orientacao, tamanho))
             j+=1
         else:
             print("Esta posição não está válida!")
 
-
-
-    
+        print(frota)
